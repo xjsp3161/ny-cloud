d19cfd9122908bf2037a8568fc145318c2777a5b
Super Wu <wuchao@cloudolp.com>
2018-06-06 10:45:20 +0800

    commit

--- common/deploy.sh
+++ common/deploy.sh
@@ -8,7 +8,7 @@ fi
 ## deploy参数，snapshot 表示快照包，简写为s， release表示正式包，简写为r
 arg=$1
 
-DEPLOY_PATH=/Users/super/Desktop/Github/maven-repo
+DEPLOY_PATH=E:\GitHub\maven-repo
 CURRENT_PATH=`pwd`
 
 deployFunc(){
@@ -19,7 +19,7 @@ deployFunc(){
   git checkout $br
   cd $CURRENT_PATH
   # 开始deploy
-  mvn clean deploy -Dmaven.test.skip  -DaltDeploymentRepository=self-mvn-repo::default::file:/Users/super/Desktop/Github/maven-repo/repository
+  mvn clean deploy -Dmaven.test.skip  -DaltDeploymentRepository=self-mvn-repo::default::file:E:\GitHub\maven-repo\repository
 
   # deploy 完成,提交
   cd $DEPLOY_PATH
